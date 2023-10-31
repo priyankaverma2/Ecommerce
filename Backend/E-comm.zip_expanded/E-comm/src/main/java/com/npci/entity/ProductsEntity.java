195a7a2daace383056a9349f0fc57e693235ff1a
madhups5 <paidasubramanyammadhu5@gmail.com>
2023-10-31 17:46:53 +0530

    adminserviceadded

--- Backend/E-comm.zip_expanded/E-comm/src/main/java/com/npci/entity/ProductsEntity.java
+++ Backend/E-comm.zip_expanded/E-comm/src/main/java/com/npci/entity/ProductsEntity.java
@@ -2,16 +2,15 @@ package com.npci.entity;
 
 
 import jakarta.persistence.Column;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
 
 @Entity
 @Table(name = "products")
-@Data
 public class ProductsEntity {
 
 	@Column(name = "product_id")
@@ -23,21 +22,103 @@ public class ProductsEntity {
 	private String name;
 	
 	@Column(name = "quantity")
-	private String quantity;
+	private int quantity;
 	
-	@Column(name = "desc")
-	private String desc;
+	@Column(name = "description")
+	private String description;
 	
 	@Column(name = "amount")
-	private String amount;
+	private double amount;
 	
 	@Column(name = "status")
-	private int status;
+	private boolean status;
 	
 	@Column(name = "image_url")
 	private String image_url;
 	
 	@Column(name = "category")
 	private String category;
-	
+
+	public ProductsEntity(int product_id, String name, int quantity, String description, double amount, boolean status,
+			String image_url, String category) {
+		super();
+		this.product_id = product_id;
+		this.name = name;
+		this.quantity = quantity;
+		this.description = description;
+		this.amount = amount;
+		this.status = status;
+		this.image_url = image_url;
+		this.category = category;
+	}
+
+	public ProductsEntity() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getProduct_id() {
+		return product_id;
+	}
+
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public String getImage_url() {
+		return image_url;
+	}
+
+	public void setImage_url(String image_url) {
+		this.image_url = image_url;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
 }
